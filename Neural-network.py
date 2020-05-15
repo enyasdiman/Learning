c2fc970d951af0bdf007c2a028467ee37fa4f8ad
EDA NUR YASDIMAN <enyasdiman@gmail.com>
2020-05-15 14:19:29 +0300

    Create Neural-network.py

---
+++ Neural-network.py
@@ -0,0 +1,15 @@
+# Import the Sequential model and Dense layer
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Create a Sequential model
+model = Sequential()
+
+# Add an input layer and a hidden layer with 10 neurons
+model.add(Dense(10, input_shape=(2,), activation="relu"))
+
+# Add a 1-neuron output layer
+model.add(Dense(1))
+
+# Summarise your model
+model.summary()
